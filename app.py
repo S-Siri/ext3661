95cc7aa4e5b20cd718dccd19a9bffe4cd17b59e4
S-Siri <123954769+S-Siri@users.noreply.github.com>
2025-12-04 14:01:34 +0530

    Update form handling and template names

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('myForm.html')
+    return render_template('form.html')
 
 @app.route('/submit',methods=['POST'])
 def submit():
-    firstname=request.form['firstname']
-    lastname=request.form['lastname']
-    age=request.form['age']
-    return render_template('greetings.html',name1=firstname,name2=lastname,age=age)
+    un=request.form['username']
+    return render_template('greeting.html',un=un)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port= 5000, debug = True)
